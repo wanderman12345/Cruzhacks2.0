2d2df6adafa13e5a96aafd7746ae303c5b1e52aa
egarc09 <95448402+egarc09@users.noreply.github.com>
2023-02-05 03:45:40 -0800

    adding files

---
+++ data/app.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from flask import Flask, url_for, request, render_template, Blueprint, redirect
+app = Flask(__name__)
+
+
+#loads csv file into 3 arrays
+df = pd.read_csv("data/list.csv")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/', methods=['POST', 'GET'])
+def search():
+    if request.method == 'POST':
+        query = request.form['query'].lower()
+        index = -1
+        for i in range(len(df)):
+            if df["Product"][i] == query:
+                print(i)
+                index = i
+    
+        if index == -1:
+            results = "Item not found"
+        else:
+            results = df["Impact"][index]
+        
+        return render_template("index.html", query=query, results=results)
+
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
